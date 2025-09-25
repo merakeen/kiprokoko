10bb20f8114b706d89f94a5bead59673860b64ee
Ikrame Saadi <hello@ikramagix.com>
2025-09-25 15:12:24 +0300

    chore(config): update site name in consts and package.json

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sorolla';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Keepro';
+export const SITE_DESCRIPTION = 'All things Cyprus, video games, sports, hidden spots, food, news : the best of pop culture';
