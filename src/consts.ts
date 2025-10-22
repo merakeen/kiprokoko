f13e38d6dc943cecf868100ee6d8c1bfa4b8a902
Ikrame Saadi <hello@ikramagix.com>
2025-10-22 23:03:12 +0300

    chore(consts): lowercase title and description for branding consistency

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Kiprokoko';
-export const SITE_DESCRIPTION = 'All things Cyprus, video games, sports, hidden spots, food, news : the best of pop culture';
+export const SITE_TITLE = 'kiprokoko';
+export const SITE_DESCRIPTION = 'all things Cyprus, video games, sports, hidden spots, food, news : the best of pop culture';
